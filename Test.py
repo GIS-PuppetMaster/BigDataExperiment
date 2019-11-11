6e9618be2fb885ab6660d0d9f326f6cccfe62036
zkx741481546 <zkx741481546@live.com>
2019-11-12 00:03:59 +0800

    修改Score算法为官网分组算法 待完成： 1、先进深度网络结构应用 2、调参 3、集成学习？

--- Test.py
+++ Test.py
@@ -23,7 +23,6 @@ def test():
     eva = model.evaluate(x_test, y_test)
     print("mae:" + str(eva[1]))
     print("acc:" + str(eva[2]))
-    y_pred_size = predict.shape[0]
 
     # 计算真实比率
     # 获取one-hot编码规则
@@ -32,22 +31,41 @@ def test():
     y_test = enc.inverse_transform(y_test)
     predict = enc.inverse_transform(predict)
 
-    y_test = Counter(np.squeeze(y_test))
-    y_pred = Counter(np.squeeze(predict))
-    for k, v in y_test.items():
-        y_test[k] = v / y_test_size
-    for k, v in y_pred.items():
-        y_pred[k] = v / y_pred_size
-
-    # 确保字典中包含所有分类
+    ans_test = None
+    ans_pred = None
     class_list = ['Fail', 'Pass', 'Good', 'Excellent']
-    for c in class_list:
-        if c not in y_pred.keys():
-            y_pred[c] = 0.0
 
-    y_test = pd.DataFrame(y_test, index=[0]).values
-    y_pred = pd.DataFrame(y_pred, index=[0]).values
-    score = 1 / (1 + 10 * mean_absolute_error(y_true=y_test, y_pred=y_pred))
+    # 将测试数据切分为组，每组大小50
+    for start in range(0, y_test_size, 50):
+        group_test = y_test[start:start + 50, :]
+        group_pred = predict[start:start + 50, :]
+        group_test_size = group_test.shape[0]
+        group_pred_size = group_pred.shape[0]
+        group_test = Counter(np.squeeze(group_test))
+        group_pred = Counter(np.squeeze(group_pred))
+        for k, v in group_test.items():
+            group_test[k] = v / group_test_size
+        for k, v in group_pred.items():
+            group_pred[k] = v / group_pred_size
+        # 确保字典中包含所有分类
+        for c in class_list:
+            if c not in group_pred.keys():
+                group_pred[c] = 0.0
+        # 构造ratio行
+        label_test = np.array(
+            [group_test['Excellent'], group_test['Good'], group_test['Pass'], group_test['Fail']]).reshape(1, 4)
+        label_pred = np.array(
+            [group_pred['Excellent'], group_pred['Good'], group_pred['Pass'], group_pred['Fail']]).reshape(1, 4)
+        # 构造ratio列表
+        if ans_test is None:
+            ans_test = label_test
+        else:
+            ans_test = np.concatenate((ans_test, label_test), axis=0)
+        if ans_pred is None:
+            ans_pred = label_pred
+        else:
+            ans_pred = np.concatenate((ans_pred, label_pred), axis=0)
+    score = 1 / (1 + 10 * mean_absolute_error(y_true=ans_test, y_pred=ans_pred))
     print("score:" + str(score))
 
 
